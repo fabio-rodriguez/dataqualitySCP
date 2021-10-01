c0484e2145b7157b6eceaffd9f6ad80de17168cb
fragnarxx <fabio25.rodriguez@gmail.com>
2021-10-01 11:03:58 +0200

    First commit: creating deliverable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dq_scp',
+      version='0.1',
+      description='Data Quality module for the SCP in the University of Seville ',
+      url='https://github.com/fabio-rodriguez/dataqualitySCP',
+      author='Fabio Rodriguez',
+      author_email='frodriguex@us.es',
+      license='own',
+      packages=['dataqualitySCP'],
+      zip_safe=False)
